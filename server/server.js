b3df87b9cf5bd2eb608c3fa666c27fb8e78ffa14
Felix Lepoutre <felix@mrflow.co>
2018-08-14 17:13:49 +0200

    add DELETE tests

--- server/server.js
+++ server/server.js
@@ -52,11 +52,12 @@ app.delete('/todos/:id', (req, res) => {
 	if (!ObjectID.isValid(id)){
 		return res.status(404).send();
 	}
+	
 	Todo.findByIdAndRemove(id).then((todo) => {
 		if (!todo){
-			return res.status(400).send();
+			return res.status(404).send();
 		}
-		return res.status(200).send(todo);
+		return res.send({todo});
 	}).catch((e) => {
 		return res.status(404).send();
 	});
