9248b65b2af3c42b8f4a950657761ada809280a2
Felix Lepoutre <felix@mrflow.co>
2018-08-21 11:24:29 +0200

    Add get users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 
-var userSchema = new mongoose.Schema({
+var UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: true,
@@ -40,7 +40,7 @@ var userSchema = new mongoose.Schema({
 //since you can't add methods to the User model. 
 //we are using the old function because it uses 'this'
 
-userSchema.methods.generateAuthToken = function () {
+UserSchema.methods.generateAuthToken = function () {
 	var user = this;
 	var access = 'Auth';
 	var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
@@ -52,13 +52,30 @@ userSchema.methods.generateAuthToken = function () {
 	});
 };
 
-userSchema.methods.toJSON = function () {
+UserSchema.statics.findByToken = function (token) {
+	var User = this;
+	var decoded;
+
+	try {
+		decoded = jwt.verify(token, 'abc123');
+	} catch (e) {
+		return Promise.reject();
+	}
+
+	return User.findOne({
+		'_id': decoded._id,
+		'tokens.token': token, 
+		'tokens.access': 'Auth'
+	});
+};
+
+UserSchema.methods.toJSON = function () {
 	var user = this;
 	var userObject = user.toObject();
 
 	return _.pick(userObject, ['_id', 'email']);
 };
 
-var User = mongoose.model('User', userSchema);
+var User = mongoose.model('User', UserSchema);
 
 module.exports = {User};
